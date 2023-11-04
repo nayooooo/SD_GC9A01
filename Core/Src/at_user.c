102e7da3ed0f9ce7617d53303d9c395207a21709
nayooooo <YEWANhup@outlook.com>
2023-11-04 17:33:30 +0800

    1. 我使用ST-LINK和USB-TTL与单片机连接，ST-LINK向单片机供电，USB-TTL仅与单片机共地 2. 我发现单独拔插以上两个器件或者拔除两个器件后，先插入USB-TTL再插入ST-LINK均正常 3. 如果拔除两个器件后，先插入ST-LINK再插入USB-TTL会重置RTC为`2000-00-00 Wednesday 00:00:00`

--- Core/Src/at_user.c
+++ Core/Src/at_user.c
@@ -6,7 +6,7 @@
 static Stream at_user_stream_device;
 static Stream* sdev = &at_user_stream_device;
 
-int _at_user_sdev_print(Stream* this, const char* message)
+static int at_user_sdev_print(Stream* this, const char* message)
 {
 	if (this == nullptr) return -1;
 	int len = strlen(message);
@@ -15,52 +15,113 @@ int _at_user_sdev_print(Stream* this, const char* message)
 	return len;
 }
 
-int _at_user_sdev_available(Stream* this)
+static int at_user_sdev_available(Stream* this)
 {
 	if (this == nullptr) return -1;
 	
 	return Get_UART1_Rx_Len();
 }
 
-int _at_user_sdev_read(Stream* this)
+static int at_user_sdev_read(Stream* this)
 {
 	if (this == nullptr) return -1;
 	return Read_UART1_Rx();
 }
 
-static At_Err_t _at_user_AT(At_Param_t param);
-static At_Err_t _at_user_AT_List(At_Param_t param);
-static At_Err_t _at_user_AT_Reboot(At_Param_t param);
-static At_Err_t _at_user_AT_Get_Time(At_Param_t param);
-static At_Err_t _at_user_AT_LED(At_Param_t param);
+static At_Err_t at_user_AT(At_Param_t param);
+static At_Err_t at_user_AT_List(At_Param_t param);
+static At_Err_t at_user_AT_Reboot(At_Param_t param);
+static At_Err_t at_user_AT_Time(At_Param_t param);
+static At_Err_t at_user_AT_LED(At_Param_t param);
 static struct At_State _atTable[] = {
-	{ "AT", AT_TYPE_CMD, _at_user_AT },
-	{ "AT+LS", AT_TYPE_CMD, _at_user_AT_List },
-	{ "AT+REBOOT", AT_TYPE_CMD, _at_user_AT_Reboot },
-	{ "AT+TIME", AT_TYPE_CMD, _at_user_AT_Get_Time },
-	{ "AT+LED", AT_TYPE_CMD, _at_user_AT_LED },
+	{ "AT", AT_TYPE_CMD, at_user_AT },
+	{ "AT+LS", AT_TYPE_CMD, at_user_AT_List },
+	{ "AT+REBOOT", AT_TYPE_CMD, at_user_AT_Reboot },
+	{ "AT+TIME", AT_TYPE_CMD, at_user_AT_Time },
+	{ "AT+LED", AT_TYPE_CMD, at_user_AT_LED },
 	{ AT_LABLE_TAIL, AT_TYPE_NULL, nullptr },
 };
 At at;
 #define AT_USER_READSTRING_LEN		(100)
 static char at_user_readString[AT_USER_READSTRING_LEN] = { 0 };
 
-static At_Err_t _at_user_AT(At_Param_t param)
+static At_Err_t at_user_AT(At_Param_t param)
 {
 	at.sendInfor(&at, AT_USER_OK);
 	return AT_EOK;
 }
 
-static At_Err_t _at_user_AT_List(At_Param_t param)
+static At_Err_t at_user_AT_List(At_Param_t param)
 {
 	at.printSet(&at, "at");
 	return AT_EOK;
 }
 
-static At_Err_t _at_user_AT_Reboot(At_Param_t param)
+static At_Err_t at_user_AT_Reboot(At_Param_t param)
 {
 	HAL_NVIC_SystemReset();
 	return AT_EOK;
+}
+
+static At_Err_t _at_user_AT_Get_Time(At_Param_t param);
+static struct At_State _at_user_Time_Table[] = {
+	{ "show", AT_TYPE_CMD, _at_user_AT_Get_Time },
+	{ AT_LABLE_TAIL, AT_TYPE_NULL, nullptr },
+};
+static At _at_Time;
+
+static At_Err_t _at_user_AT_Get_Time(At_Param_t param)
+{
+	const char* weekDay[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+	RTC_DateTypeDef date;
+	RTC_TimeTypeDef time;
+	HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN);
+	HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
+	_at_Time.printf(&_at_Time, "%04d-%02d-%02d %s %02d:%02d:%02d\r\n",
+		(int)date.Year - RTC_USER_DATE_YEAR_DELTA, date.Month, date.Date, weekDay[date.WeekDay],
+		time.Hours, time.Minutes, time.Seconds);
+	return AT_EOK;
+}
+
+static At_Err_t at_user_AT_Time(At_Param_t param)
+{
+	if (!param->argc) {
+		_at_Time.printSet(&_at_Time, "at Time");
+		at.sendInfor(&at, AT_USER_ERROR_ARGC);
+		return AT_ERROR;
+	}
+	
+	size_t len = 0;
+	for (int i = 0; i < param->argc; i++) {
+		len += strlen(param->argv[i]) + 1;
+	}
+	len -= 1;
+	char* str = (char*)at_malloc(len + 1);
+	if (str == nullptr) {
+		at.sendInfor(&at, AT_USER_ERROR_MALLOC);
+		return AT_ERROR;
+	}
+	at_memset(str, 0, len + 1);
+	size_t cpy_num = 0;
+	for (int i = 0; i < param->argc; i++) {
+		at_memcpy(str + cpy_num, param->argv[i], strlen(param->argv[i]));
+		cpy_num += strlen(param->argv[i]);
+		str[cpy_num] = ' ';
+		cpy_num += 1;
+	}
+	str[len] = 0;
+	At_Err_t err = _at_Time.handle(&_at_Time, str);
+	at_free(str);
+	
+	if (err == AT_EOK) {
+		at.sendInfor(&at, AT_USER_OK);
+	} else if (err == AT_ERROR_NOT_FIND) {
+		_at_Time.printSet(&_at_Time, "at LED");
+		at.sendInfor(&at, AT_USER_ERROR_NOTFIND);
+	} else {
+		at.sendInfor(&at, AT_USER_ERROR_ARGV);
+	}
+	return err;
 }
 
 static At_Err_t _at_user_AT_LED_on(At_Param_t param);
@@ -84,20 +145,7 @@ static At_Err_t _at_user_AT_LED_off(At_Param_t param)
 	return AT_EOK;
 }
 
-static At_Err_t _at_user_AT_Get_Time(At_Param_t param)
-{
-	const char* weekDay[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-	RTC_DateTypeDef date;
-	RTC_TimeTypeDef time;
-	HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN);
-	HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
-	at.printf(&at, "%04d-%02d-%02d %s %02d:%02d:%02d\r\n",
-		(int)date.Year - RTC_USER_DATE_YEAR_DELTA, date.Month, date.Date, weekDay[date.WeekDay],
-		time.Hours, time.Minutes, time.Seconds);
-	return AT_EOK;
-}
-
-static At_Err_t _at_user_AT_LED(At_Param_t param)
+static At_Err_t at_user_AT_LED(At_Param_t param)
 {
 	if (param->argc != 1) {
 		_at_LED.printSet(&_at_LED, "at LED");
@@ -129,10 +177,12 @@ static At_Err_t _at_user_AT_LED(At_Param_t param)
 
 At_Err_t at_user_init(void)
 {
-	At_Err_t err = Stream_Init(sdev, 3, _at_user_sdev_print, _at_user_sdev_available, _at_user_sdev_read);
+	At_Err_t err = Stream_Init(sdev, 3, at_user_sdev_print, at_user_sdev_available, at_user_sdev_read);
 	if (err != AT_EOK) return err;
 	err = At_Init(&at, _atTable, sdev, sdev, at_user_readString, AT_USER_READSTRING_LEN, 0);
 	if (err != AT_EOK) return err;
 	err = At_Create(&_at_LED, _at_user_LED_Table, sdev, sdev, 1, 10);
+	if (err != AT_EOK) return err;
+	err = At_Create(&_at_Time, _at_user_Time_Table, sdev, sdev, 1, 10);
 	return err;
 }
